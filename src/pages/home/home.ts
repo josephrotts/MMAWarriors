23dd57c4a2b72987fdd2f5e0548d5f50c2bce92f
FUSION\nikola.trajkovic <nikola.trajkovic@bluecouchdigital.com>
2018-01-23 16:43:42 +0100

    Added Products slider and Product list on Home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
 
 import * as WC from 'woocommerce-api';
 
@@ -10,16 +10,46 @@ import * as WC from 'woocommerce-api';
 export class HomePage {
   WooCommerce: any;
   products: any[];
+  moreProducts: any;
+  page: number;  
+
+  @ViewChild('productSlides') productSlides: Slides;
 
   constructor(public navCtrl: NavController) {
+    this.page = 2;
+
     this.WooCommerce = WC({                           // WC takes json obj as parametar
         url: "http://localhost/wordpress/",
         consumerKey: "ck_13a1af926accdeac39e2cd1c2f0dde80de7a569a",
         consumerSecret: "cs_4517d5811e5ef68521ab3b5426ff48abae3716f4"
     });
 
+    this.loadMoreProducts();
+
     this.WooCommerce.getAsync("products").then( (data) => {
       this.products = JSON.parse(data.body).products;
+    }, (err) => {
+      console.log(err)
+    })
+  }
+
+  /**
+   * This is afunction which is
+   * executed every time when 
+   * page is loaded. 
+   */
+  ionViewDidLoad() {
+    setInterval(() => {
+      if (this.productSlides.getActiveIndex() == this.productSlides.length() - 1)
+        this.productSlides.slideTo(0);
+
+      this.productSlides.slideNext();                 // this will be executed every 3 sec
+    }, 3000)
+  }
+
+  loadMoreProducts() {
+    this.WooCommerce.getAsync("products?page=" + this.page).then( (data) => {     // this: "products?page=" + this.page  = will get the products from the second page, which means the next 10 products will be loaded/retrived from woocommerce store.  
+      this.moreProducts = JSON.parse(data.body).products;
     }, (err) => {
       console.log(err)
     })
