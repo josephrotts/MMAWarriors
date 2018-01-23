6db638a71ddcfcbe375d146fb3957fe4ed3146bc
FUSION\nikola.trajkovic <nikola.trajkovic@bluecouchdigital.com>
2018-01-23 01:03:07 +0100

    Displaying Products on Home page from WooCommerce API

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import * as WC from 'woocommerce-api';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  WooCommerce: any;
+  products: any[];
 
   constructor(public navCtrl: NavController) {
+    this.WooCommerce = WC({                           // WC takes json obj as parametar
+        url: "http://localhost/wordpress/",
+        consumerKey: "ck_13a1af926accdeac39e2cd1c2f0dde80de7a569a",
+        consumerSecret: "cs_4517d5811e5ef68521ab3b5426ff48abae3716f4"
+    });
 
+    this.WooCommerce.getAsync("products").then( (data) => {
+      this.products = JSON.parse(data.body).products;
+    }, (err) => {
+      console.log(err)
+    })
   }
 
 }
